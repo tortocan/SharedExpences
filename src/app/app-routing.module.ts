12677d375217698c06f643e2a7da42441ae11b59
gtortocan <george.tortocan@gmail.com>
2022-05-12 11:56:00 +0200

    Add docker and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BalanceComponent } from './balance/balance.component';
+import { ExpensesComponent } from './expenses/expenses.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'prefix' },
+  { path: 'home', component: HomeComponent },
+  { path: 'balance', component: BalanceComponent },
+  { path: 'expenses', component: ExpensesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExpensesComponent } from './expenses/expenses.component';
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { BalanceComponent } from './balance/balance.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpensesComponent,
+    HomeComponent,
+    NavigationComponent,
+    FooterComponent,
+    BalanceComponent
   ],
   imports: [
     BrowserModule,
